6d35044dbafdccdf82ca5f16473caf81ee844251
Oliver Chase <olichase91@gmail.com>
2023-04-04 14:17:51 -0700

    Solved Lab

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 /*
-  You should be able to call this function with no arguments and see its output in the terminal:
+  You should be able to call this function
+  with no arguments and see its 
+  output in the terminal:
   greetProgrammer();
   => "Hello, programmer!"
 */
@@ -8,7 +10,9 @@ function greetProgrammer() {
 }
 
 /*
-  You should be able to call this function with one argument and see its output in the terminal:
+  You should be able to call this function
+  with one argument and see its output 
+  in the terminal:
   greet("Naureen");
   => "Hello, Naureen!"
 */
@@ -17,11 +21,15 @@ function greet(name) {
 }
 
 /*
-  You should be able to call this function with no arguments and see its output in the terminal:
+  You should be able to call this function 
+  with no arguments and see its output in 
+  the terminal:
   greetWithDefault();
   => "Hello, programmer!"
 
-  You should also be able to call this function with one argument and see its output in the terminal:
+  You should also be able to call this 
+  function with one argument and see its 
+  output in the terminal:
   greetWithDefault("Sunny");
   => "Hello, Sunny!"
 */
@@ -30,7 +38,8 @@ function greetWithDefault(name = "programmer") {
 }
 
 /*
-  You should be able to call this function with two arguments and get back its return value:
+  You should be able to call this function 
+  with two arguments and get back its return value:
   const sum = add(1, 2);
   console.log(sum);
   => 3
@@ -40,12 +49,14 @@ function add(num1, num2) {
 }
 
 /*
-  You should be able to call this function with two arguments and get back its return value:
+  You should be able to call this function
+  with two arguments and get back its return value:
   const result = halve(4);
   console.log(result);
   => 2
 
-  If the function is called with an argument that isn't a number, it should return null:
+  If the function is called with an argument 
+  that isn't a number, it should return null:
   const result = halve("two")
   => null
 */
--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+print(my_function(1))
+
+def stylish_painter():
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    return best_hairstyle
+    print(best_hairstyle)
+
+print(stylish_painter())
+
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if (number != int(number)): return None
+    else: return number / 2 
+
+print(halve(4))
